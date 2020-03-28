b3e0418b3c6706479641fc8b88291c2ab7ef26ad
alipinski15 <58602252+alipinski15@users.noreply.github.com>
2020-03-27 21:52:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ const append_page_links = (list) => {
     page_div.appendChild(new_div);
     new_div.appendChild(ul);
 
-//This loop creates an 'LI' element for every 10 students. Sets an Anchor with the 'href' of '#' and a page number.     
+    //This loop creates an 'LI' element for every 10 students. Sets an Anchor with the 'href' of '#' and a page number.     
     for(let i = 0; i < page_list; i++){
       let li = document.createElement('li');
       let anchor = document.createElement('a');
@@ -43,9 +43,11 @@ const append_page_links = (list) => {
       if(i === 0){
          anchor.className = 'active';
       }
-// Creates clickable page Buttons. Based on the amount of students, each page will display the appropriate amount needed. 
+
+      // Creates clickable page Buttons. Based on the amount of students, each page will display the appropriate amount needed. 
       anchor.addEventListener('click', (e) => {
          const li_active = document.querySelectorAll('.pagination a');
+
          for(let j = 0; j < li_active.length; j++){
             li_active[j].classList.remove('active');
          } 
@@ -60,4 +62,39 @@ show_page(student_list, 1);
 append_page_links(student_list);
 
 
+const page_header = document.querySelector('.page-header');
+const search_div = document.createElement('div');
+search_div.className = 'student-search';
+page_header.appendChild(search_div);
+const search_input = document.createElement('input');
+search_input.setAttribute('placeholder', 'Search for Students...')
+search_div.appendChild(search_input);
+const search_button = document.createElement('button');
+search_button.textContent = 'Search';
+search_div.appendChild(search_button);
 
+const search_results = [];
+
+const student_search = (searchInput, names) => {
+   const search_results = [];
+
+   for(let i = 0; i < names.length; i++){
+      let searched = names[i];
+      searched.style.display = 'none';
+      searched.classList.remove('match');
+
+      if(searchInput.value.length !== 0 && searched.textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
+         searched.className = 'match';
+         searched.style.display = 'block';
+         search_results.push(searched);
+      }
+   }
+   append_page_links(search_results);
+   show_page(search_results, 1);
+}
+
+search_button.addEventListener('click', (event) => {
+   event.preventDefault();
+   student_search(search_input, student_list);
+   console.log(search_results);
+ });
